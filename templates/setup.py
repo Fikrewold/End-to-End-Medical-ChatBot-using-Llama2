ec6d490644a3356605b45fa1a7a002b7080f970d
Fikre <fikre.wold@gmail.com>
2025-02-14 22:19:21 -0600

    model instruction added

---
+++ templates/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Fikre Bitew',
+    author_email= 'fikre.wold@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
